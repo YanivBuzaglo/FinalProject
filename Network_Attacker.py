c65f4b9e4cc7cbdfd39034a0411aafa19009a0a0
YanivBuzaglo <yaniv5231@gmail.com>
2022-07-15 04:42:43 +0300

    .

--- Network_Attacker.py
+++ Network_Attacker.py
@@ -1,8 +1,8 @@
-from pickletools import OpcodeInfo
+from sys import flags
 from scapy.all import *
 from scapy.layers.inet import TCP, IP, Ether, ICMP
 from scapy.layers.l2 import ARP
-packet1 = (IP(dst="192.168.1.1")/TCP(dport=80, flags="S"))
+packet1 = (IP(dst="192.168.1.49")/TCP(dport=80, flags="S"))
 ans, unans = sr(packet1)
 print("------------------------------------")
 print(ans)
